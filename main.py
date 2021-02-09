183e4c1cf30bc1d6fd763f70ea8ab7ff3cd08d0e
saxenabhishek <as7122000@gmail.com>
2021-02-09 18:47:11 +0530

    Feat(main) : ploting graphs

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ Main Driver Code
 """
 
 import ML.train
+import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    ML.train.train_automate(1)
+    R, F = ML.train.train_automate(1)
+    plt.plot(R)
+    plt.plot(F)
+    plt.show()
