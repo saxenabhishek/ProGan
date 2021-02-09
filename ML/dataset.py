f46a1ecb738cdb5b407790acd709213235a46da6
saxenabhishek <as7122000@gmail.com>
2021-02-09 15:27:23 +0530

    feat(ml/dataset) : Split in ratio

--- ML/dataset.py
+++ ML/dataset.py
@@ -12,9 +12,6 @@ class Data:
     def __init__(
         self,
         path,
-        trainset,
-        testset,
-        valset,
         mean=(0.5, 0.5, 0.5),
         std=(0.5, 0.5, 0.5),
         size=(176, 176),
@@ -28,10 +25,6 @@ class Data:
         self.batch_size = batch_size
         self.shuffle = shuffle
         self.num_workers = num_workers
-        self.trainset = trainset
-
-        self.valset = valset
-        self.testset = testset
 
         self.transforms = transforms.Compose(
             [
@@ -43,23 +36,50 @@ class Data:
 
         self.folderdata = ImageFolder(self.path, transform=self.transforms)
 
-    def getdata(self):
+    def getdata(self, split):
+
+        assert len(split) == 3
+
+        divisor = 0
+        for i in range(3):
+            divisor += split[i]
+
+        length = len(self.folderdata)
+
+        # 1 is the samllest unit we can divide the dataset in.
+        #  without the if condition it might be 0
+        unit = int(length / divisor) if divisor < length else 1
+
+        # modifying split to be scaled according to legth of dataset
+        split[0] = split[0] * unit
+        split[1] = split[1] * unit
+
+        split[2] = length - split[0] - split[1]
 
         train, test, val = random_split(
             self.folderdata,
-            [self.trainset, self.testset, self.valset],
-            generator=torch.Generator().manual_seed(42),
+            split,
+            generator=torch.Generator().manual_seed(69),
         )
 
         trainloader = DataLoader(
-            train, shuffle=True, batch_size=self.batch_size, num_workers=2
+            train,
+            shuffle=True,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
         )
         testloader = DataLoader(
-            test, shuffle=True, batch_size=self.batch_size, num_workers=2
+            test,
+            shuffle=True,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
         )
 
         valloader = DataLoader(
-            val, shuffle=True, batch_size=self.batch_size, num_workers=2
+            val,
+            shuffle=True,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
         )
 
         return trainloader, testloader, valloader
@@ -71,8 +91,7 @@ class Data:
 
 
 if __name__ == "__main__":
-    data = Data(
-        path="../../fashiondata/img", trainset=50000, testset=2000, valset=712
-    )
-    train, test, val = data.getdata()
-
+    data = Data(path="Data_small", batch_size=12)
+    split = [20, 1, 1]
+    train, test, val = data.getdata(split)
+    print(len(train), len(test), len(val))
