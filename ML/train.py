231162bf1949d9c86ae83db72b792fad4c1c10c0
saxenabhishek <as7122000@gmail.com>
2021-02-10 22:45:58 +0530

    fix(*) : aradhya can't git

--- ML/train.py
+++ ML/train.py
@@ -1,5 +1,7 @@
 """
+
 Training
+
 """
 
 import torch
@@ -13,55 +15,56 @@ import matplotlib.pyplot as plt
 from time import time
 
 
+class train:
+    def __init__(
+        self, path, epochs, batch_size, split, display_step=50, vec_shape=100, noisedim=100, savedir="ModelWeights"
+    ):
 
-class train():
-    def __init__(self, path, epochs, batch_size,split,
-                 vec_shape=100, noisedim=100 ,savedir='ModelWeights'):
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.gen = Generator(device=self.device, noise_dim=noisedim, vec_shape=vec_shape).to(self.device)
+        self.disc = Discriminator().to(self.device)
+        self.resnet = ResNetEncoder(vec_shape=vec_shape).to(self.device)
+        self.epochs = epochs
+        self.display_step = display_step
+        self.root = savedir + "/"
+        self.criterion = nn.BCEWithLogitsLoss()
+        beta1 = 0.5
+        lr = 0.002
+        self.discopt = optim.Adam(self.disc.parameters(), lr=lr, betas=(beta1, 0.999))
+        self.genopt = optim.Adam(
+            list(self.resnet.parameters()) + list(self.gen.parameters()), lr=lr, betas=(beta1, 0.999)
+        )
+        data = Data(path=path, batch_size=batch_size, size=(64, 64))
+        self.trainloader, self.testloader, _ = data.getdata(split=split)
 
-                self.device = "cuda" if torch.cuda.is_available() else "cpu"
-                self.gen = Generator(device=self.device, noise_dim=noisedim, vec_shape=vec_shape).to(self.device)
-                self.disc = Discriminator().to(self.device)
-                self.resnet = ResNetEncoder(vec_shape=vec_shape).to(self.device)
-                self.epochs = epochs
+        self.gen = self.gen.apply(self.weights_init)
+        self.disc = self.disc.apply(self.weights_init)
 
-                self.root = savedir
-                self.criterion = nn.BCEWithLogitsLoss()
-                beta1 = 0.5
-                lr = 0.002
-                self.discopt = optim.Adam(self.disc.parameters(), lr=lr, betas=(beta1, 0.999))
-                self.genopt = optim.Adam(list(self.resnet.parameters()) + list(self.gen.parameters()),lr=lr, betas=(beta1, 0.999))
-                data = Data(path=path, batch_size=batch_size)
-                self.trainloader, self.testloader, _  = data.getdata(split=split)
-
-                self.gen = self.gen.apply(self.weights_init)
-                self.disc = self.disc.apply(self.weights_init)
-
-                self.discLosses = []
-                self.genLosses = []
-        
+        self.discLosses = []
+        self.genLosses = []
 
     def trainer(self):
-        
+
         self.gen.train()
         self.disc.train()
 
         cur_step = 0
-        
-        display_step = 500
+        display_step = self.display_step
         mean_discriminator_loss = 0
         mean_generator_loss = 0
+        testimage = next(iter(self.testloader))
+        testimage = testimage[0].to(self.device)
 
         for epoch in range(self.epochs):
             print("training")
             for image, _ in tqdm(self.trainloader):
                 ## training disc
-                
+
                 image = image.to(self.device)
                 self.discopt.zero_grad()
                 discrealout = self.disc(image)
                 vector = self.resnet(image)
                 discfakeout = self.disc(self.gen(vector).detach())
-
 
                 realdiscloss = self.criterion(discrealout, torch.ones_like(discrealout))
                 fakediscloss = self.criterion(discfakeout, torch.zeros_like(discfakeout))
@@ -78,18 +81,15 @@ class train():
                 genout = self.disc(self.gen(vector))
                 genoutloss = self.criterion(genout, torch.ones_like(genout))
 
-
                 genoutloss.backward()
-                
+                mean_generator_loss += genoutloss.item() / display_step
                 self.genopt.step()
-                mean_generator_loss += genoutloss.item() / display_step 
 
-                
+                # print(cur_step)
                 if cur_step % display_step == 0 and cur_step > 0:
-                    print(f"Step {cur_step}: Generator loss: {mean_generator_loss}, \t discriminator loss: {mean_discriminator_loss}")
-                    
-                    testimage = next(iter(self.testloader))
-                    testimage = testimage[0]
+                    print(
+                        f"Step {cur_step}: Generator loss: {mean_generator_loss}, \t discriminator loss: {mean_discriminator_loss}"
+                    )
                     fake = self.gen(self.resnet(testimage))
                     self.show_tensor_images(fake)
                     self.show_tensor_images(testimage)
@@ -99,8 +99,8 @@ class train():
 
                     mean_generator_loss = 0
                     mean_discriminator_loss = 0
-            
-            cur_step += 1
+
+                cur_step += 1
 
             print("Saving weights")
 
@@ -114,6 +114,7 @@ class train():
         image_unflat = image_tensor.detach().cpu()
         image_grid = make_grid(image_unflat[:num_images], nrow=5)
         plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+        plt.axis(False)
         plt.show()
 
     def weights_init(self, m):
@@ -122,14 +123,16 @@ class train():
         if isinstance(m, nn.BatchNorm2d):
             torch.nn.init.normal_(m.weight, 0.0, 0.02)
             torch.nn.init.constant_(m.bias, 0)
-    
+
     def plot_trainer(self):
-        assert len(self.discLosses) != 0 and len(self.genLosses != 0)
-        plt.plot(self.discLosses, label = 'Discriminator Loss')
-        plt.plot(self.genLosses, label = 'Generator Loss')
+        assert len(self.discLosses) != 0 and len(self.genLosses) != 0
+        plt.plot(self.discLosses, label="Discriminator Loss")
+        plt.plot(self.genLosses, label="Generator Loss")
         plt.legend()
         plt.show()
 
+
 if __name__ == "__main__":
-    train = train('../../fashiondata/img', epochs=1, batch_size=100, vec_shape=100, split=[20, 1,0])
+    train = train("../../fashiondata/img", epochs=1, batch_size=100, vec_shape=100)
     train.trainer()
+
