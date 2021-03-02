a613039909a785287af7c95b0f16234d2faa8fdf
Aditya Shukla <adityashukzy@gmail.com>
2021-03-02 12:25:21 +0530

    Fix(main): Increase display step

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from time import time
 
 def main(path, epochs, split=[1, 2000, 0]):
     modeltrain = train(
-        path=path, epochs=epochs, batch_size=4, vec_shape=100, split=split, noisedim=100, display_step=1
+        path=path, epochs=epochs, batch_size=4, vec_shape=100, split=split, noisedim=100, display_step=100
     )
     starting = time()
     modeltrain.trainer()
