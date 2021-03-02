ff18beac25f02a812a0ec0b25cd93e73e113c0f7
Aditya Shukla <adityashukzy@gmail.com>
2021-03-02 11:55:13 +0530

    Fix(main): Add args to main

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 from time import time
 
 
-def main():
+def main(path, epochs, split=[1, 2000, 0]):
     modeltrain = train(
-        path="Data", epochs=1, batch_size=4, vec_shape=100, split=[1, 2000, 0], noisedim=100, display_step=1
+        path=path, epochs=epochs, batch_size=4, vec_shape=100, split=split, noisedim=100, display_step=1
     )
     starting = time()
     modeltrain.trainer()
