69059c98e03b95affb6df6af3d76647291a9c784
Aditya Shukla <adityashukzy@gmail.com>
2021-03-02 19:48:36 +0530

    Feat(main): Add display_step param

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import matplotlib.pyplot as plt
 from time import time
 
 
-def main(path, epochs, split=[1, 2000, 0]):
+def main(path, epochs, split=[1, 2000, 0], display_step=100):
     modeltrain = train(
-        path=path, epochs=epochs, batch_size=4, vec_shape=100, split=split, noisedim=100, display_step=100
+        path=path, epochs=epochs, batch_size=4, vec_shape=100, split=split, noisedim=100, display_step=display_step
     )
     starting = time()
     modeltrain.trainer()
