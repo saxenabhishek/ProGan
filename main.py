2c214ca68ecd15cd71d0cd755f58a9ad8265fb3d
saxenabhishek <as7122000@gmail.com>
2021-05-15 04:33:35 +0530

    feat(main) : demo file

--- main.py
+++ main.py
@@ -2,21 +2,41 @@
 Main Driver Code
 
 """
+from ML.proTrain import train
 
-from ML.train import train
-import matplotlib.pyplot as plt
-from time import time
+import torch
+
+torch.autograd.set_detect_anomaly(True)
 
 
-def main(path, epochs, split=[1, 2000, 0], display_step=250):
-    modeltrain = train(
-        path="Data", epochs=3, batch_size=10, vec_shape=100, split=[1, 20, 0], noisedim=100, display_step=10000
+def main():
+    gan = train(
+        "Data",
+        128, #batch size
+        [90, 10, 0], #data split
+        "ModelWeights", #save location
+        lr=[0.001, 0.001], #learning Rates
+        merge_samples_Const=17, # merge rate
+        loadmodel=True, # continue training
+        PlotInNotebook=False, # if in notebook
     )
-    starting = time()
-    modeltrain.trainer()
-    print(time() - starting)
-    modeltrain.plot_trainer()
-
+    gan.step_up()  # -> 8,8
+    gan.trainer(17, 100)
+    gan.plot_trainer()
+    gan.trainer(17, 100)
+    gan.trainer(17, 100)
+    gan.step_up()  # -> 16,16
+    gan.trainer(17, 100)
+    gan.trainer(17, 100)
+    gan.step_up()  # -> 32,32
+    gan.trainer(17, 100)
+    gan.trainer(17, 100)
+    gan.step_up()  # -> 64,64
+    gan.trainer(17, 100)
+    gan.trainer(17, 100)
+    gan.step_up()  # -> 128,128
+    gan.trainer(17, 100)
+    gan.trainer(17, 100)
 
 if __name__ == "__main__":
     main()
