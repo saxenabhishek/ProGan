158455e180724d2bbeece2458087b21ce38df832
saxenabhishek <as7122000@gmail.com>
2021-05-16 01:54:39 +0530

    fix(demo) : formating

--- main.py
+++ main.py
@@ -2,22 +2,23 @@
 Main Driver Code
 
 """
-from ML.proTrain import trainer
+from progan.proTrain import trainer
 
 import torch
+
 torch.autograd.set_detect_anomaly(True)
 
 
 def main():
     gan = trainer(
-        "Data",                 # path to data
-        128,                    # batch size
-        [90, 10, 0],            # data split
-        "ModelWeights",         # save location
-        lr=[0.001, 0.001],      # learning Rates
-        merge_samples_Const=17, # merge rate
-        loadmodel=True,         # continue training
-        PlotInNotebook=False,   # if in notebook
+        "Data",  # path to data
+        128,  # batch size
+        [90, 10, 0],  # data split
+        "ModelWeights",  # save location
+        lr=[0.001, 0.001],  # learning Rates
+        merge_samples_const=17,  # merge rate
+        loadmodel=True,  # continue training
+        PlotInNotebook=False,  # if in notebook
     )
     gan.train(17, 100)
     gan.step_up()  # -> 8,8
@@ -35,6 +36,7 @@ def main():
     gan.step_up()  # -> 128,128
     gan.train(17, 100)
     gan.train(17, 100)
+
 
 if __name__ == "__main__":
     main()
