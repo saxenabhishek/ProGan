c3d72e17f5ddc3d3890e8c69dfdc8486fa3352a7
saxenabhishek <as7122000@gmail.com>
2021-05-15 16:41:02 +0530

    fix(demo) : wrong call

--- main.py
+++ main.py
@@ -2,41 +2,39 @@
 Main Driver Code
 
 """
-from ML.proTrain import train
+from ML.proTrain import trainer
 
 import torch
-
 torch.autograd.set_detect_anomaly(True)
 
 
 def main():
-    gan = train(
-        "Data",
-        128, #batch size
-        [90, 10, 0], #data split
-        "ModelWeights", #save location
-        lr=[0.001, 0.001], #learning Rates
+    gan = trainer(
+        "Data",                 # path to data
+        128,                    # batch size
+        [90, 10, 0],            # data split
+        "ModelWeights",         # save location
+        lr=[0.001, 0.001],      # learning Rates
         merge_samples_Const=17, # merge rate
-        loadmodel=True, # continue training
-        PlotInNotebook=False, # if in notebook
+        loadmodel=True,         # continue training
+        PlotInNotebook=False,   # if in notebook
     )
+    gan.train(17, 100)
     gan.step_up()  # -> 8,8
-    gan.trainer(17, 100)
-    gan.plot_trainer()
-    gan.trainer(17, 100)
-    gan.trainer(17, 100)
+    gan.train(17, 100)
+    gan.train(17, 100)
     gan.step_up()  # -> 16,16
-    gan.trainer(17, 100)
-    gan.trainer(17, 100)
+    gan.train(17, 100)
+    gan.train(17, 100)
     gan.step_up()  # -> 32,32
-    gan.trainer(17, 100)
-    gan.trainer(17, 100)
+    gan.train(17, 100)
+    gan.train(17, 100)
     gan.step_up()  # -> 64,64
-    gan.trainer(17, 100)
-    gan.trainer(17, 100)
+    gan.train(17, 100)
+    gan.train(17, 100)
     gan.step_up()  # -> 128,128
-    gan.trainer(17, 100)
-    gan.trainer(17, 100)
+    gan.train(17, 100)
+    gan.train(17, 100)
 
 if __name__ == "__main__":
     main()
